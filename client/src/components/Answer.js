23439e0b96635056968ef5edd51235865ff169a7
Ash Conery <aconery@gmail.com>
2021-02-26 13:34:52 -0800

    final answer updates

--- client/src/components/Answer.js
+++ client/src/components/Answer.js
@@ -1,71 +1,30 @@
-<<<<<<< HEAD
-import react, {Component} from 'react';
-//option #1//
-function Answer() {
 
-    clickHandler = (event) => {
+import React from 'react';
+// import './answer.scss';
+
+
+function Answer({id, name}) {
+
+    function clickHandler () {
         console.log('button clicked');
-        event.preventDefault();
+        var element = document.querySelector("#myList");
+        element.classList.toggle("character-name__frame");
+        document.querySelector("#pokepic").classList.toggle("toggle-img");
     }
     return (
 
         <div className="full-answer">
             <h1>Did you guess right?</h1>
+
             <button onClick={clickHandler} className="button">Get Answer</button>
-             <ul>
-                 <li className="character-name"><h2>{props.picture}</h2></li>
-                <li className="character-name"><h2>{props.name}</h2></li>
+
+             <ul id="myList" className="character-name__frame">
+                <li className="character-name"><h2>{name}</h2></li>
             </ul> 
         </div>
     )
 }
 
-//option #2//
-class GiveAnswer extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            picture: '',
-            name:'',
-        }
-        this.clickHandlerTwo = this.clickHandlerTwo.bind (this)
-    }
-
-    clickHandlerTwo() {
-        this.setState({
-           picture:{this.name.picture},
-           name:{this.name.name}, 
-        })
-        console.log(this)
-    }
-    render (){
-        return (
-            <div>
-                <div>{this.state.picture}</div>
-                <div>{this.state.name}</div>
-                <button onClick={this.clickHandlerTwo}>Get Answer</button>
-            </div>
-        )
-    }
-}
-
-
-
-// export default Answer; 
-// export default GiveAnswer;
-=======
-import react from "react";
-
-function Answer(props) {
-  return (
-    <ul className="full-answer">
-      <li key={props.id} className="character-name">
-        <h2>{props.name}</h2>
-      </li>
-    </ul>
-  );
-}
-
 export default Answer;
->>>>>>> 99ec530d726b9d0f913814504fdc8559331d0481
+
+
