2524655b0c9a44dc15694379cd704e9e685a00a0
Ash Conery <aconery@gmail.com>
2021-02-26 11:45:49 -0800

    answer file update with two options

--- client/src/components/Answer.js
+++ client/src/components/Answer.js
@@ -1,9 +1,55 @@
-import react from 'react';
+import react, {Component} from 'react';
+//option #1//
+function Answer() {
 
-function Answer (props) {
+    clickHandler = (event) => {
+        console.log('button clicked');
+        event.preventDefault();
+    }
     return (
-        <ul className="full-answer">
-            <li key={props.id} className="character-name"><h2>{props.name}</h2></li>
-        </ul>
+
+        <div className="full-answer">
+            <h1>Did you guess right?</h1>
+            <button onClick={clickHandler} className="button">Get Answer</button>
+             <ul>
+                 <li className="character-name"><h2>{props.picture}</h2></li>
+                <li className="character-name"><h2>{props.name}</h2></li>
+            </ul> 
+        </div>
     )
 }
+
+//option #2//
+class GiveAnswer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            picture: '',
+            name:'',
+        }
+        this.clickHandlerTwo = this.clickHandlerTwo.bind (this)
+    }
+
+    clickHandlerTwo() {
+        this.setState({
+           picture:{this.name.picture},
+           name:{this.name.name}, 
+        })
+        console.log(this)
+    }
+    render (){
+        return (
+            <div>
+                <div>{this.state.picture}</div>
+                <div>{this.state.name}</div>
+                <button onClick={this.clickHandlerTwo}>Get Answer</button>
+            </div>
+        )
+    }
+}
+
+
+
+// export default Answer; 
+// export default GiveAnswer;
