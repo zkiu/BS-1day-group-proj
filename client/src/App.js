a05d45484a6f8bd146d8b116ab327d728e6a3d86
Kiu <zqdummy@gmail.com>
2021-02-26 14:02:51 -0800

    final commit from Kiu

--- client/src/App.js
+++ client/src/App.js
@@ -8,48 +8,47 @@ const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 let randomPokemonId = 0;
 
 class App extends React.Component {
-  state = {
-    picture: "",
-    name: "",
-    id: "",
-  };
+	state = {
+		picture: "",
+		name: "",
+		id: "",
+	};
 
-  getRandomId() {
-    return Math.floor(Math.random() * Math.floor(151));
-  }
-  componentDidMount() {
-    this.getPokemon();
-  }
+	getRandomId() {
+		return Math.floor(Math.random() * Math.floor(151));
+	}
+	componentDidMount() {
+		this.getPokemon();
+	}
 
-  getPokemon() {
-    randomPokemonId = this.getRandomId();
-    this.getPokemonData(randomPokemonId);
-  }
-  reloadPokemon() {
-    this.getPokemon();
-  }
-  getPokemonData = (id) => {
-    axios
-      .get(apiUrl + id)
-      .then((respone) => {
-        this.setState({
-          picture: respone.data.sprites.other["official-artwork"].front_default,
-          name: respone.data.name,
-          id: respone.data.id,
-        });
-      })
-      .catch((error) => console.log(error));
-  };
-  render() {
-    return (
-      <div className="App">
-        <Picture picture={this.state.picture} />
-        <Answer name={this.state.name} id={this.state.id} /> 
-        <h1> {this.state.name}</h1>
-        <button onClick={this.reloadPokemon.bind(this)}>Refresh</button> 
-      </div>
-    );
-  }
+	getPokemon() {
+		randomPokemonId = this.getRandomId();
+		this.getPokemonData(randomPokemonId);
+	}
+	reloadPokemon() {
+		this.getPokemon();
+	}
+	getPokemonData = (id) => {
+		axios
+			.get(apiUrl + id)
+			.then((respone) => {
+				this.setState({
+					picture: respone.data.sprites.other["official-artwork"].front_default,
+					name: respone.data.name,
+					id: respone.data.id,
+				});
+			})
+			.catch((error) => console.log(error));
+	};
+	render() {
+		return (
+			<div className="App">
+				<Picture picture={this.state.picture} />
+				<Answer name={this.state.name} id={this.state.id} />
+				<button onClick={this.reloadPokemon.bind(this)}>Refresh</button>
+			</div>
+		);
+	}
 }
 
 export default App;
