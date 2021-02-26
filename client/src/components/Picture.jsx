0f8bf5c904afb71d82bcdbea3b64844a5d12ef27
Kiu <zqdummy@gmail.com>
2021-02-26 11:04:47 -0800

    minor picture tweak

--- client/src/components/Picture.jsx
+++ client/src/components/Picture.jsx
@@ -2,6 +2,7 @@ export default function Picture({ picture }) {
 	return (
 		<div className="img-container">
 			<img
+				id="pokepic"
 				className="img-container__img toggle-img"
 				src={picture}
 				alt="pokemon"
