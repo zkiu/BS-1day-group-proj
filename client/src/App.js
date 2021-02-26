ff79b179b8919ab4dc558d214600d53a8a3d336a
Dex <dxneufeld14@gmail.com>
2021-02-26 13:16:40 -0800

    added refresh button

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import axios from "axios";
 import Picture from "./components/Picture";
-import Answer from "./components/Answer";
+// import Answer from "./components/Answer";
 
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 let randomPokemonId = 0;
@@ -18,14 +18,20 @@ class App extends React.Component {
     return Math.floor(Math.random() * Math.floor(151));
   }
   componentDidMount() {
+    this.getPokemon();
+  }
+
+  getPokemon() {
     randomPokemonId = this.getRandomId();
     this.getPokemonData(randomPokemonId);
+  }
+  reloadPokemon() {
+    this.getPokemon();
   }
   getPokemonData = (id) => {
     axios
       .get(apiUrl + id)
       .then((respone) => {
-        console.log(respone.data.sprites.other["official-artwork"]);
         this.setState({
           picture: respone.data.sprites.other["official-artwork"].front_default,
           name: respone.data.name,
@@ -38,7 +44,9 @@ class App extends React.Component {
     return (
       <div className="App">
         <Picture picture={this.state.picture} />
-        <Answer name={this.state.name} id={this.state.id} />
+        {/* <Answer name={this.state.name} id={this.state.id} /> */}
+        <h1> {this.state.name}</h1>
+        <button onClick={this.reloadPokemon.bind(this)}>Refresh</button>
       </div>
     );
   }
